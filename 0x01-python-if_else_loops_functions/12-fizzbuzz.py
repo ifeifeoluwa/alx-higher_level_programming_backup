11a495f38697a9c264860f4e455f28f6f97c9979
Ife <ife.ifeoluwa@gmail.com>
2023-03-14 05:40:13 -0700

    function that prints the numbers from 1 to 100 separated by a space.

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for counter in range(1, 101):
+    if counter % 3 == 0 and counter % 5 == 0:
+        status = "FizzBuzz"
+    elif counter % 3 == 0:
+    	status = "Fizz"
+    elif counter % 5 == 0:
+    	status = "Buzz"
+    else:
+    	status = counter
+    print("{}".format(status), end=" ")
