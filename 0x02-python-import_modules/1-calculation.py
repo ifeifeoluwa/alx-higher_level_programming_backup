7656d11cb6244c1a9e3a16b41776b505419b7c6f
Ife <ife.ifeoluwa@gmail.com>
2023-03-16 13:13:17 -0700

    few updates

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# program imports functions from the file calculator_1.py,
-# does some Maths, and prints the result.
-
 if __name__ == "__main__":
     
     from calculator_1 import add, sub, mul, div
