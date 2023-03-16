1d75a885755c095fb60fd4f3b01a26a73ca6544f
Ife <ife.ifeoluwa@gmail.com>
2023-03-16 14:36:52 -0700

    program that imports the variable a from the file variable_load_5.py and prints its value.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" program that imports the variable a from the file 
+""" program that imports the variable a from the file
     variable_load_5.py and prints its value.    """
 
 if __name__ == "__main__":
