151583e8fd37ace127343f71a0ae0bf80149bc6a
Ife <ife.ifeoluwa@gmail.com>
2023-03-16 13:20:46 -0700

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+""" this program imports functions from the file calculator_1.py,
+    does some Maths, and prints the result. """
+
 if __name__ == "__main__":
-    
+
     from calculator_1 import add, sub, mul, div
 
     a = 10
