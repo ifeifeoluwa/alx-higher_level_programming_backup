7418c1003d4cd6cdb9615541d77e7f6223111f5f
Ife <ife.ifeoluwa@gmail.com>
2023-03-18 06:59:24 -0700

    function that returns a tuple with the length of a string and its first character.

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" finds the biggest integer of a list.
+                                        """
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return "None"
+    else:
+        maxi = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > maxi:
+                maxi = my_list[i]
+        return maxi 
