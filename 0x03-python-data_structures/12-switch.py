480f94efa244a4ee67748ebb6d3120beea415109
Ife <ife.ifeoluwa@gmail.com>
2023-03-20 15:23:17 -0700

    function to switch value of a and b

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
-"""
-    This function switches value of a and b
-    """
-
-
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        del my_list[idx]
-    return my_list
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
