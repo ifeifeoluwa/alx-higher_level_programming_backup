71d2ce08c7541f48d055d6fd7e6bf7f6d8c52b12
Ife <ife.ifeoluwa@gmail.com>
2023-04-13 05:43:01 -0700

    function that divides all elements of a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -20,7 +20,7 @@ def matrix_divided(matrix, div):
         elif size != len(k):
             raise TypeError("Each row of the matrix must have the same size")
         for i in k:
-            if type(k) is not int and type(i) is not float:
+            if type(i) is not int and type(i) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
     if type(div) is not int and type(div) is not float:
